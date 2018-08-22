12845cb2a808c25881cfce629bdaaf9cec8fffc3
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-22 22:18:47 +0200

    Changed code, transition working

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ var technologyLeft = document.getElementById('technology-left');
 });
 }*/
 
-var box1 = document.getElementById('box1');
+/*var box1 = document.getElementById('box1');
 var box2 = document.getElementById('box2');
 var box3 = document.getElementById('box3');
 var box4 = document.getElementById('box4');
 var box5 = document.getElementById('box5');
 var box6 = document.getElementById('box6');
 var box7 = document.getElementById('box7');
-var box8 = document.getElementById('box8');
+var box8 = document.getElementById('box8');*/
 
 
-var changePosition = box => {
-    document.querySelector(box).classList.toggle('size');
+/*var changePosition = box => {
     document.querySelector(box).classList.toggle('box');
-}
+    document.querySelector(box).classList.toggle('position');
+}*/
 
 var addDisplay = (hide, show) => {
     document.querySelector(hide).classList.add('hide')
     document.querySelector(show).classList.remove('hide');
 }
 
-
+/*  
 box1.addEventListener('click', event => {
     event.preventDefault;
     changePosition('#box1');
@@ -73,15 +73,13 @@ box7.addEventListener('click', event => {
 box8.addEventListener('click', event => {
     event.preventDefault;
     changePosition('#box8');
-})
-
+})*/
 
 
 firstRight.addEventListener('click', event => {
     event.preventDefault();
     addDisplay('.first', '.second');
 })
-
 firstLeft.addEventListener('click', event => {
     event.preventDefault;
     addDisplay('.second', '.first');
@@ -89,7 +87,7 @@ firstLeft.addEventListener('click', event => {
 
 aboutRight.addEventListener('click', event => {
     event.preventDefault;
-    addDisplay('.second','.about');
+    addDisplay('.second', '.about');
 })
 
 aboutLeft.addEventListener('click', event => {
