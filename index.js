a703f150524f014f1572f9488e39ecae410d96b2
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-22 15:59:26 +0200

    Added projects to show in the middle

--- index.js
+++ index.js
@@ -5,13 +5,77 @@ var aboutLeft = document.getElementById('about-left');
 var projectsRight = document.getElementById('projects-right');
 var projectsLeft = document.getElementById('projects-left');
 var technologyRight = document.getElementById('technology-right');
-var technologyLeft = document.getElementById('technology-left')
+var technologyLeft = document.getElementById('technology-left');
 
+//var boxes = document.querySelectorAll('.box1');
+
+/*for(let i=0; i<boxes.length; i++) {
+    box[i].document.getElementById('click', event =>{
+        event.preventDefault;
+        changePosition('.box[i]');
+});
+}*/
+
+var box1 = document.getElementById('box1');
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+var box4 = document.getElementById('box4');
+var box5 = document.getElementById('box5');
+var box6 = document.getElementById('box6');
+var box7 = document.getElementById('box7');
+var box8 = document.getElementById('box8');
+
+
+var changePosition = box => {
+    document.querySelector(box).classList.toggle('size');
+    document.querySelector(box).classList.toggle('box');
+}
 
 var addDisplay = (hide, show) => {
     document.querySelector(hide).classList.add('hide')
     document.querySelector(show).classList.remove('hide');
 }
+
+
+box1.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box1');
+})
+
+box2.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box2');
+})
+
+box3.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box3');
+})
+
+box4.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box4');
+})
+box5.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box5');
+})
+
+box6.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box6');
+})
+box7.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box7');
+})
+
+box8.addEventListener('click', event => {
+    event.preventDefault;
+    changePosition('#box8');
+})
+
+
 
 firstRight.addEventListener('click', event => {
     event.preventDefault();
